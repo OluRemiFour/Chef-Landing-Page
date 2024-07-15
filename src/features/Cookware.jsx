fdfd85a1155c6b7c2030fe1b7107fe8d47e7926a
OluRemiFour <lekanayoola04@gmail.com>
2024-07-15 02:19:17 -0700

    lenght

--- src/features/Cookware.jsx
+++ src/features/Cookware.jsx
@@ -2,111 +2,8 @@ import { useState } from "react";
 import Spinner from "../../public/Spinner.gif";
 import { BsCart2 } from "react-icons/bs";
 import { MdFavoriteBorder } from "react-icons/md";
-// import Hero from "../components/Hero";
 
 function Cookware({ handleAddToCart, imageBaseUrl, cookware }) {
-  {
-    /* <Hero /> */
-  }
-  {
-    /* <div className="lg:mx-16 my-14">
-        <div className="pb-12">
-          <h1 className="font-bold text-3xl py-5 text-center mx-auto w-fit">
-            Cookware Collection
-          </h1>
-          <img src="/Line.png" alt="line" className="mx-auto w-fit" />
-        </div>
-
-        <div>
-          <div className="flex items-center justify-between m-8">
-            <p className="">6 items found </p>
-            <div className="border p-2">
-              {" "}
-              Sort by:
-              <select
-                name=""
-                id=""
-                className="text-[#DB6A18] px-1 font-semibold text-sm outline-none"
-              >
-                <option value="">Popularity</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="lg:m-8 m-4 space-y-4 md:space-y-0 md:flex gap-4 justify-between">
-            {!cookware ? (
-              <img
-                src={Spinner}
-                className="fixed w-fit flex justify-center items-center text-center fill-black right-[50%] top-[50%] mx-auto"
-              />
-            ) : (
-              <div className="lg:mx-16 my-8">
-                <div className="lg:m-8 m-4 space-y-4 md:space-y-0 md:flex gap-4 justify-between">
-                  <div className="md:grid md:grid-cols-3 gap-4 space-y-4 md:space-y-0">
-                    {cookware.map((cookware, index) => (
-                      <div
-                        key={index}
-                        className="flex flex-col space-y-[12px] border rounded-md py-8 px-4 w-fit"
-                      >
-                        <div>
-                          {cookware.photos && cookware.photos.length > 0 && (
-                            <img
-                              src={`${imageBaseUrl}${cookware.photos[0].url}`}
-                              alt={cookware.name}
-                              className="size-full"
-                            />
-                          )}
-                          <div className="bg-[#DB6A18]  rounded-full mt-[-3.4rem] flex mr-auto mx-4 absolute opacity-[88px] w-[40px] h-[40px] items-center justify-center">
-                            <MdFavoriteBorder className="text-white size-6" />
-                          </div>
-                        </div>
-
-                        <div className="flex justify-between font-semibold">
-                          <h1>{cookware.name}</h1>
-                          <p>N190</p>
-                        </div>
-                        <h3 className="text-[14px] w-fit">
-                          This 5-quart pot is crafted from high-quality, durable
-                          materials that ensure even heat distribution and
-                          long-lasting performance.
-                        </h3>
-                        <div className="flex items-center justify-between">
-                          <p className="font-semibold">Available colors</p>
-                          <div className="flex gap-2">
-                            <div className="bg-[#E0DFFE] font-semibold rounded-full border ring ring-gray-500 h-[18px] w-[18px]"></div>
-                            <div className="bg-[#5754BA] rounded-full h-[18px] w-[18px]"></div>
-                            <div className="bg-[#DEAE55] rounded-full h-[18px] w-[18px]"></div>
-                          </div>
-                        </div>
-                        <div className="flex justify-between pt-4 items-center">
-                          <div>
-                            <p className="font-semibold">Sizes in set</p>
-                            <div className="flex gap-2">
-                              <p>12</p>
-                              <p>20</p>
-                              <p>24</p>
-                            </div>
-                          </div>
-
-                          <button
-                            onClick={() => handleAddToCart(cookware)}
-                            className="flex gap-2 border p-2 rounded-md border-black items-center"
-                          >
-                            <p>Add to cart</p>
-                            <BsCart2 />
-                          </button>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div> */
-  }
-
   const itemsPerPage = 3; // Number of items per page
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -130,7 +27,7 @@ function Cookware({ handleAddToCart, imageBaseUrl, cookware }) {
 
         <div>
           <div className="flex items-center justify-between m-8">
-            <p>{cookware.length} items found</p>
+            <p>{cookware?.length} items found</p>
             <div className="border p-2">
               Sort by:
               <select
@@ -160,7 +57,7 @@ function Cookware({ handleAddToCart, imageBaseUrl, cookware }) {
                         className="flex flex-col space-y-[12px] border rounded-md py-8 px-4 w-fit"
                       >
                         <div>
-                          {cookware.photos && cookware.photos.length > 0 && (
+                          {cookware.photos && cookware.photos?.length > 0 && (
                             <img
                               src={`${imageBaseUrl}${cookware.photos[0].url}`}
                               alt={cookware.name}
--- src/features/Knives.jsx
+++ src/features/Knives.jsx
@@ -1,111 +1,6 @@
-// import { BsCart2 } from "react-icons/bs";
-// import { MdFavoriteBorder } from "react-icons/md";
-// import Spinner from "../../public/Spinner.gif";
-// import { useState } from "react";
-
-// function Knives({ knives, imageBaseUrl, handleAddToCart, cartCounter }) {
-//   // const [page, setPage] = useState();
-//   return (
-//     <>
-//       {!knives ? (
-//         <img src={Spinner} />
-//       ) : (
-//         <div className="lg:mx-16 my-14">
-//           <div className="pb-12">
-//             <h1 className="font-bold text-3xl py-5 text-center mx-auto w-fit">
-//               Knives Collection
-//             </h1>
-//             <img src="/Line.png" alt="line" className="mx-auto w-fit" />
-//           </div>
-
-//           <div>
-//             <div className="flex items-center justify-between m-8">
-//               <p className="">6 items found </p>
-//               <div className="border p-2">
-//                 {" "}
-//                 Sort by:
-//                 <select
-//                   name=""
-//                   id=""
-//                   className="text-[#DB6A18] px-1 font-semibold text-sm outline-none"
-//                 >
-//                   <option value="">Popularity</option>
-//                 </select>
-//               </div>
-//             </div>
-
-//             <div className="lg:m-8 m-4 space-y-4 md:space-y-0 md:flex gap-4 justify-between">
-//               <div className="md:grid md:grid-cols-3 gap-4 space-y-4 md:space-y-0">
-//                 {knives.map((knife, index) => (
-//                   <div
-//                     key={index}
-//                     className="flex flex-col space-y-[12px] border rounded-md py-8 px-4 w-fit"
-//                   >
-//                     <div>
-//                       {knife.photos && knife.photos.length > 0 && (
-//                         <img
-//                           src={`${imageBaseUrl}${knife.photos[0].url}`}
-//                           alt={knife.name}
-//                           className="size-full"
-//                         />
-//                       )}
-//                       <div className="bg-[#DB6A18]  rounded-full mt-[-3.4rem] flex mr-auto mx-4 absolute opacity-[88px] w-[40px] h-[40px] items-center justify-center">
-//                         <MdFavoriteBorder className="text-white size-6" />
-//                       </div>
-//                     </div>
-
-//                     <div className="flex justify-between font-semibold">
-//                       <h1>{knife.name}</h1>
-//                       <p>N190</p>
-//                     </div>
-//                     <h3 className="text-[14px] w-fit">
-//                       This 5-quart pot is crafted from high-quality, durable
-//                       materials that ensure even heat distribution and
-//                       long-lasting performance.
-//                     </h3>
-//                     <div className="flex items-center justify-between">
-//                       <p className="font-semibold">Available colors</p>
-//                       <div className="flex gap-2">
-//                         <div className="bg-[#E0DFFE] font-semibold rounded-full border ring ring-gray-500 h-[18px] w-[18px]"></div>
-//                         <div className="bg-[#5754BA] rounded-full h-[18px] w-[18px]"></div>
-//                         <div className="bg-[#DEAE55] rounded-full h-[18px] w-[18px]"></div>
-//                       </div>
-//                     </div>
-//                     <div className="flex justify-between pt-4 items-center">
-//                       <div>
-//                         <p className="font-semibold">Sizes in set</p>
-//                         <div className="flex gap-2">
-//                           <p>12</p>
-//                           <p>20</p>
-//                           <p>24</p>
-//                         </div>
-//                       </div>
-
-//                       <button
-//                         onClick={() => handleAddToCart(knife)}
-//                         className="flex gap-2 border p-2 rounded-md border-black items-center"
-//                       >
-//                         <p>Add to cart</p>
-//                         <BsCart2 />
-//                       </button>
-//                     </div>
-//                   </div>
-//                 ))}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-// export default Knives;
-
 import { useState } from "react";
 import { BsCart2 } from "react-icons/bs";
 import { MdFavoriteBorder } from "react-icons/md";
-import { Link } from "react-router-dom";
 
 function ProductList({ knives, handleAddToCart, imageBaseUrl }) {
   const itemsPerPage = 3;
@@ -154,7 +49,7 @@ function ProductList({ knives, handleAddToCart, imageBaseUrl }) {
             className="flex flex-col space-y-[12px] border rounded-md py-8 px-4 w-fit"
           >
             <div>
-              {knife.photos && knife.photos.length > 0 && (
+              {knife.photos && knife.photos?.length > 0 && (
                 <img
                   src={`${imageBaseUrl}${knife.photos[0].url}`}
                   alt={knife.name}
